30fadeb87f1f7caf2c3d36d22e08233f133eff34
Rick Frank <rfrank@dominionsw.com>
2018-12-04 13:33:30 -0500

    remove deprecated code

--- ColorSegmentation.m
+++ ColorSegmentation.m
@@ -1,22 +1,20 @@
 clear all;
 
-location1 = 'D:\Projects\GreenStand\ImageData';
+location1 = 'D:\Projects\GreenStand\ImageData\NeedColorSegmentation';
 thumbnails = 'D:\Projects\GreenStand\ImageData\Thumbnails';
 
 segmented = 'D:\Projects\GreenStand\ImageData\ColorSegmented';
-pine = 'D:\Projects\GreenStand\ImageData\Pinus_Pendula';
-templateNeedles = imread('templateF.tif');
- 
+  
 ds = datastore({location1},'Type','image','FileExtensions',{'.jpg','.tif','.png'});
 output = [];
 [dsRows,dsCols] = size(ds.Files);
 wb = waitbar(0);
 
-gpuTemplateNeedles = gpuArray(templateNeedles);
+ 
  
 for i = 1:dsRows
     try
-        waitbar(i/dsRows);
+ 
         filePath = ds.Files(i);
         
         data = readimage(ds,i);
@@ -26,17 +24,6 @@ for i = 1:dsRows
         f = fullfile(segmented,strcat(name,'SEG','.tif'));
         imwrite(SC,f);
         
-        % try to find pinus_pendula
-        imageGray = rgb2gray(SC);
-        gpuImageGray = gpuArray(imageGray);
-        gpuMatch = normxcorr2(gpuTemplateNeedles,gpuImageGray);
-        match = gather(gpuMatch);
-       
-        f = fullfile(segmented,strcat(name,'TEMPLATEMATCH','.tif'));
-  
-        imwrite(match,f);
-       
-      
     catch ME
         continue;
     end
--- deprecated/RotZ.m
+++
@@ -1,6 +0,0 @@
-function [ M ] = RotZ( theta )
-M = [ cos(theta) -sin(theta)  0
-    sin(theta)  cos(theta)  0
-    0           0       1];
-end
-
